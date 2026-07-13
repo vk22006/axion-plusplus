95a360a7a6dd6800f8c70991f5197d43cee7f274
vk22006 <kishorev22006@gmail.com>
2026-07-13 23:26:11 +0530

    config: Set current version details and basic config

---
+++ include/axion/config.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace axion {
+    using Scalar = double;
+    using Index = size_t;
+}
---
+++ include/axion/version.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace axion {
+    constexpr int VERSION_MAJOR = 0;
+    constexpr int VERSION_MINOR = 1;
+    constexpr int VERSION_PATCH = 0;
+
+    constexpr const char* VERSION = "0.1.0";
+}
