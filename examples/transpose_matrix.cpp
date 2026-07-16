1a495b96bc6d88a8bbce8dd501c6c77e800bf7e0
vk22006 <kishorev22006@gmail.com>
2026-07-16 16:06:35 +0530

    doc: Implemented working example on usecase of axion::Matrix::transposeOf()

---
+++ examples/transpose_matrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <axion/matrix.hpp>
+
+int main() {
+    std::cout << axion::VERSION << "\n";
+
+    std::vector<std::vector<double>> data = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    axion::Matrix A(data);
+
+    std::vector<std::vector<double>> t = A.transposeOf(data);
+    A.display(t);
+
+    std::cout << "Rows = " << A.rowCount() << "\n";
+    std::cout << "Cols = " << A.colCount();
+
+    return 0;
+}
